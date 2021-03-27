43e36244ccb375493ed5c25a1c944930925fabbf
Rafael Pereira <rafael.bernardes@live.com>
2021-03-27 16:20:10 -0300

    Inclui endpoint

---
+++ src/main/java/br/com/gateway/api/rest/ImpostoTerritorialResource.java
@@ -0,0 +1,53 @@
+package br.com.gateway.api.rest;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.Response.Status.Family;
+
+import org.jboss.resteasy.client.jaxrs.ResteasyClient;
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+
+import br.com.gateway.api.util.PropertiesUtil;
+
+@Path("espelhos")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ImpostoTerritorialResource implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@GET
+	public Response getImpostoTerritorial(@QueryParam("documento") String documento, @QueryParam("tipo-pessoa") Integer tipoPessoa) throws IllegalArgumentException, NullPointerException, IOException {
+		ResteasyClient client = new ResteasyClientBuilder().build();
+		
+		WebTarget target = client.target(PropertiesUtil.obterURI("stur-imposto-territorial-api")).path("espelhos");
+		
+		target = target.queryParam("documento", documento);
+		target = target.queryParam("tipo-pessoa", tipoPessoa);
+		
+		Response response = target.request().get();
+		
+		String resposta = "";
+		
+		if(Family.SUCCESSFUL.equals(response.getStatusInfo().getFamily())) {
+			resposta = response.readEntity(String.class);
+			
+			return Response.ok(resposta).build();
+		}else {
+			return Response.status(Status.fromStatusCode(response.getStatus())).build();
+		}
+		
+		
+	}
+
+}
